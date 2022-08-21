66d63c690fd21bd0f7a44c116e9d453b73923887
TysonR0319 <tysonr0319@gmail.com>
2022-08-21 22:50:07 +0000

    fixed layout error

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
-import { Home } from "./pages/home";
+import { Home } from "./View/home";
 // import navbarDark from "./component/navbarDark";
 import injectContext from "./store/appContext";
 
