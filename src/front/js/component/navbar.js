e20e530eac5ff2d9a6e7ab9acd926aac0abf771d
dimaredunlap <dimaredunlap@gmail.com>
2022-08-23 23:45:53 +0000

    created navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,5 +1,19 @@
 import React from "react";
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
-  return <h2>Recipe Page Navbar</h2>;
+  return (
+    <div className="container-fluid">
+      <h2>Logo</h2>
+      <input class="search" type="text" placeholder="Search.."></input>
+      <a href="#category" class="category">Category</a>
+      <a href="#Contact" class="contact">Contact</a>
+      <button type="button" class="btn btn-dark register">
+        Register
+      </button>
+      <button type="button" class="btn btn-dark login">
+        Login
+      </button>
+    </div>
+  );
 };
