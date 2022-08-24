afae94a19e53951efc820bf83543c5812db8a87d
dimaredunlap <dimaredunlap@gmail.com>
2022-08-24 20:23:23 +0000

    carousel added

--- src/front/js/component/carousel.js
+++ src/front/js/component/carousel.js
@@ -12,21 +12,21 @@ export const Carousel = () => {
         <div className="carousel-item active">
           <img
             src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-instant-pot-pot-roast-horizontal-1545249595.jpg?crop=0.617xw:0.925xh;0.0785xw,0&resize=640:*"
-            className="d-block w-100"
+            className="d-block"
             alt="..."
           ></img>
         </div>
         <div className="carousel-item">
           <img
             src="https://www.eatthis.com/wp-content/uploads/sites/4/2022/05/steak-n-shake-7x7-steakburger.jpg?quality=82&strip=1"
-            className="d-block w-100"
+            className="d-block"
             alt="..."
           ></img>
         </div>
         <div className="carousel-item">
           <img
             src="https://colonydiner.com/wp-content/uploads/2021/03/French.jpg"
-            className="d-block w-100"
+            className="d-block"
             alt="..."
           ></img>
         </div>
