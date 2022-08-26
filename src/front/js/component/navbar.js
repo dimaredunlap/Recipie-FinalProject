39780e88b7aadeca79349268beee0f7e53bafde7
dimaredunlap <dimaredunlap@gmail.com>
2022-08-26 17:25:48 +0000

    fixed className error

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -16,26 +16,26 @@ export const Navbar = () => {
       {/* Dark/Light mode button */}
     <div className="navMoon" style={{backgroundImage: `url(${moon})`}}>
     </div>
-      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-      <span class="navbar-toggler-icon"></span>
+      <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+      <span className="navbar-toggler-icon"></span>
       </button>
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Foodgasm</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div className="offcanvas-header">
+        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Foodgasm</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-evenly flex-grow-1 pe-3">
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="#">Meal Type's</a>
+      <div className="offcanvas-body">
+        <ul className="navbar-nav justify-content-evenly flex-grow-1 pe-3">
+          <li className="nav-item">
+            <a className="nav-link" aria-current="page" href="#">Meal Types</a>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Conctact Us!
             </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Email</a></li>
-              <li><a class="dropdown-item" href="#">Slack</a></li>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href="#">Email</a></li>
+              <li><a className="dropdown-item" href="#">Slack</a></li>
               <li>
                 
               </li>
