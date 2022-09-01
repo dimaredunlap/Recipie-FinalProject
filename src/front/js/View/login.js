e12feaed423c767cfcb55ce5c005b628356e0460
dimaredunlap <dimaredunlap@gmail.com>
2022-09-01 19:45:48 +0000

    some styling

--- src/front/js/View/login.js
+++ src/front/js/View/login.js
@@ -8,49 +8,51 @@ let email = "";
 let password = "";
 
 export const Login = () => {
+  let navigate = useNavigate();
 
-    let navigate = useNavigate();
-
-    const userLogin = async () => {
-        try {
-            const user = {
-                email: email,
-                password: password,
-            };
-            const response = await LoginUser(user);
-            const data = await response.json();
-            if (response.status === 200) {
-                localStorage.setItem("token", data);
-            }
-        } catch (err) {
-            console.log("Login error: ", err);
-        } finally {
-            navigate("/home")
-        }
-
+  const userLogin = async () => {
+    try {
+      const user = {
+        email: email,
+        password: password,
+      };
+      const response = await LoginUser(user);
+      const data = await response.json();
+      if (response.status === 200) {
+        localStorage.setItem("token", data);
+      }
+    } catch (err) {
+      console.log("Login error: ", err);
+    } finally {
+      navigate("/home");
     }
+  };
 
   return (
-    <div className="text-center mt-5">
-      <h1>Login</h1>
+    <div className="text-center container col-3 position-absolute top-50 start-50 translate-middle">
+      <h1 className="mb-4">Login</h1>
       <div>
-      <input
-        type="text"
-        className="login-input p-3 form-control"
-        placeholder="Email"
-        onChange={(e) => email = e.target.value.toLowerCase()}
-      />
-      <input
-        type="password"
-        className="login-input p-3 mb-2 mt-3 form-control"
-        placeholder="Password"
-        onChange={(e) => password = e.target.value}
-      />
-      <button
-        type="submit"
-        className="login-button mb-2 mt-3"
-        onClick={() => {userLogin();}}>Login
-      </button>
+        <input
+          type="text"
+          className="login-input p-3 form-control mt-8"
+          placeholder="Email"
+          onChange={(e) => (email = e.target.value.toLowerCase())}
+        />
+        <input
+          type="password"
+          className="login-input p-3 mb-2 mt-3 form-control"
+          placeholder="Password"
+          onChange={(e) => (password = e.target.value)}
+        />
+        <button
+          type="submit"
+          className="login-button mb-2 mt-3"
+          onClick={() => {
+            userLogin();
+          }}
+        >
+          Login
+        </button>
       </div>
     </div>
   );
