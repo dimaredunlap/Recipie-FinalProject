3311693ec90c97fb6c99e610e0a50ae0a2cfa162
dimaredunlap <dimaredunlap@gmail.com>
2022-09-03 15:12:04 +0000

    styling

--- src/front/js/View/breakfast.js
+++ src/front/js/View/breakfast.js
@@ -11,11 +11,12 @@ export const Breakfast = () => {
   return (
     <div className="text-center mt-5">
       <h1>Breakfast</h1>
+      <p>It's the most important meal of the day! Check out these tasty and delicious recipes!</p>
       <Row xs={1} md= {2} lg={4} className="g-4 container">
         {Array.from({ length: 4 }).map((_, idx) => (
           <Col>
             <Card>
-              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Img className="image" variant="top" src="..." />
               <Card.Body>
                 <Card.Title>Card title</Card.Title>
                 <Card.Text>
