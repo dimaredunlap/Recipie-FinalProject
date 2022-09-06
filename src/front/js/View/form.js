f99a6a2d5bc9d5251606f5f5054a5739a47ae06d
TysonR0319 <tysonr0319@gmail.com>
2022-09-06 23:06:28 +0000

    added some styling

--- src/front/js/View/form.js
+++ src/front/js/View/form.js
@@ -10,42 +10,42 @@ export  const FormPage = () => {
           
             <form class="row g-8 mb-100" />
 
-            <div class="rtitle">
+            <div class="rtitle mt-4">
               <label for="rtitle">Recipe Title:</label>
               <input class="form-control input-sm" id="rtitle" type="text" />
             </div>
 
-            <div class="nos">
+            <div class="nos mt-2">
               <label for="servings">Number of Servings:</label>
               <input class="form-control input-sm" id="servings" type="text" />
             </div>
 
-            <div class="ing">
+            <div class="ing mt-2">
               <label for="ingre">Ingredients:</label>
               <textarea class="form-control input-sm" id="ingre" rows="3" type="textarea" />
             </div>
 
-            <div class="direct">
+            <div class="direct mt-2">
               <label for="directions">Directions:</label>
               <textarea class="form-control input-sm" id="directions" rows="3" type="textarea" />
             </div>
 
-            <div class="preptime">
+            <div class="preptime mt-2">
               <label for="preptime">Prep Time:</label>
               <input class="form-control input-sm" id="preptime" type="text" />
             </div>
 
-            <div class="cooktime">
+            <div class="cooktime mt-2">
               <label for="cooktime">Cook time:</label>
               <input class="form-control input-sm" id="cooktime" type="text" />
             </div>
 
-            <div class="totaltime">
+            <div class="totaltime mt-2">
               <label for="totaltime">Total Time:</label>
               <input class="form-control input-sm" id="totaltime" type="text" />
             </div>
 
-            <div class="credit">
+            <div class="credit mt-2">
               <label for="credit">Credit:</label>
               <input class="form-control input-sm" id="credit" type="text" />
             </div>
@@ -53,7 +53,7 @@ export  const FormPage = () => {
             <div>
             <form className="mt-2">
 
-              <label for="menu type">Meal Type:</label>
+              <label className="pe-2" for="menu type">Type of meal:</label>
               <select name="mealtype" id="mealtype">
                 <option value="breakfast">Breakfast</option>
                 <option value="lunch">Lunch</option>
@@ -69,8 +69,8 @@ export  const FormPage = () => {
           </div>
 
 
-            <label class="customFile" for="customFile">Submit Photo Here</label>
-            <input type="file" class="form-control  btn my-3" id="customFile" />
+            <label class="customFile mt-3" for="customFile">Submit Photo Here</label>
+            <input type="file" class="form-control my-3" id="customFile" />
             <div>
             <button className="btn my-3">Submit</button>
             </div>
