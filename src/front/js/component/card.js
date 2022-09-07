eb177088c1a8edbd55e9cf11c1bfc7fcf37ac385
dimaredunlap <dimaredunlap@gmail.com>
2022-09-07 17:31:15 +0000

    some styling

--- src/front/js/component/card.js
+++ src/front/js/component/card.js
@@ -11,13 +11,8 @@ export const Card = (props) => {
       <div className="card-image d-flex flex-row">
         <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-instant-pot-pot-roast-horizontal-1545249595.jpg?crop=0.617xw:0.925xh;0.0785xw,0&resize=640:*" className="card-img" alt="..." />
         <div className="card-body">
-          {/* <h5 className="card-title">{props.name}</h5> */}
           <p className="card-text">Card Title</p>
-          <div className="card-end">
-            <button>
-              <i className="bi bi-heart-fill"></i>
-            </button>
-          </div>
+          <button>SUBMIT</button>
         </div>
       </div>
     </div>
