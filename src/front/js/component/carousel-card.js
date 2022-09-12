4e1a7eeafde7e5a39e539cfb588321e0ece2269c
TysonR0319 <tysonr0319@gmail.com>
2022-09-12 23:09:21 +0000

    fixed alignment of button

--- src/front/js/component/carousel-card.js
+++ src/front/js/component/carousel-card.js
@@ -10,7 +10,7 @@ function CarouselCard() {
                     <div className="card-body  small-card-body">
                         <p className="h2">Card Tittle</p>
                         <p className='p-small-card'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, aliquam!</p>
-                        <a href="/" className="btn btn-small reg-btn btn-grad">Check recepi out!</a>
+                        <a href="/" className="btn btn-small reg-btn btn-grad">Check recipe out!</a>
                     </div>
                 </div>
             </div>
