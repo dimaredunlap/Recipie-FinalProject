25271eed030355c324e919344a165eaad7799af3
TysonR0319 <tysonr0319@gmail.com>
2022-09-16 01:00:50 +0000

    added comments

--- src/front/js/View/grocerylist.js
+++ src/front/js/View/grocerylist.js
@@ -3,10 +3,14 @@ import { Context } from "../store/appContext";
 import { SelectedIngredients } from "../component/selectedIngredients";
 
 export const Glist = () => {
+  {//JS Create a store for the grocerylist, when the checkbox is checked it will push that ingridient to the flux store. We will then call it here, pass through props.
+  }
   let ingredients=["onions","cheese","meat","bun"]
   return (
     <div>
       <h1>
+        {//Recipe tittle
+        }
         List 
       </h1>
       <SelectedIngredients ingredients={ingredients}/>
