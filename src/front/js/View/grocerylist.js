f059a130f4bf516ef61fce1910edcfe7ce009d39
idhanani786 <irshandhanani786@gmail.com>
2022-09-17 16:28:21 +0000

    tbufin

--- src/front/js/View/grocerylist.js
+++ src/front/js/View/grocerylist.js
@@ -1,14 +1,21 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import { SelectedIngredients } from "../component/selectedIngredients";
+import "../../styles/SelectedIngredients.css"
+
 
 export const Glist = () => {
   {//JS Create a store for the grocerylist, when the checkbox is checked it will push that ingridient to the flux store. We will then call it here, pass through props.
   }
+
+
+  
   let ingredients=["onions","cheese","meat","bun"]
+
+  
   return (
-    <div>
-      <h1>
+    <div class="container recebox">
+      <h1 class="text-start">
         {//Recipe tittle
         }
         List 
--- src/front/js/component/selectedIngredients.js
+++ src/front/js/component/selectedIngredients.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import "../../styles/SelectedIngredients.css";
 
 export const SelectedIngredients = (props) => {
+  const [hovered, setHovered] = useState(false);
+  const toggleHover = () => setHovered(!hovered);
+
   return (
-    <div className="container">
+    <div className="col-5">
       <ul class="list-group">
         {props.ingredients.map((ingredient) => (
-          <li class="list-group-item">{ingredient}</li>
+          <li class="list-group-item">
+            {ingredient}
+            <button
+              className={hovered ? "visible": "invisible"}
+              onMouseEnter={toggleHover}
+              onMouseLeave={toggleHover}
+            ><i class="bi bi-trash"></i>
+            </button>
+          </li>
         ))}
       </ul>
     </div>
