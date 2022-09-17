cdd8a37519ee2722c33058c64875144f0b9cb0c6
dimaredunlap <dimaredunlap@gmail.com>
2022-09-17 14:44:30 +0000

    added link card to single view

--- src/front/js/View/mealType.js
+++ src/front/js/View/mealType.js
@@ -5,6 +5,8 @@ import "../../styles/mealTypes.css";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import { Card } from "../component/card.js";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 export const MealType = (props) => {
   const { store, actions } = useContext(Context);
@@ -14,9 +16,11 @@ export const MealType = (props) => {
       <h1>{props.title}</h1>
       <Row className="container">
         {props.mtype.map((recipe, idx) => (
-          <Col xs={12} className="container my-3">
-            <Card key={idx} recipe={recipe}/>
-          </Col>
+          <Link to={`/singleview/${idx+1}`} className="link">
+            <Col xs={12} className="container my-3">
+              <Card key={idx} recipe={recipe} />
+            </Col>
+          </Link>
         ))}
       </Row>
     </div>
