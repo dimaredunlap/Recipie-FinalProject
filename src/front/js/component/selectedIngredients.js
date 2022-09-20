95c5488801e51352480b2120144a083db2e93bd2
idhanani786 <irshandhanani786@gmail.com>
2022-09-20 20:29:51 +0000

    trashicon

--- src/front/js/component/selectedIngredients.js
+++ src/front/js/component/selectedIngredients.js
@@ -1,22 +1,33 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "../../styles/SelectedIngredients.css";
 
 export const SelectedIngredients = (props) => {
   const [hovered, setHovered] = useState(false);
-  const toggleHover = () => setHovered(!hovered);
+  const [ingredients, setIngredients] = useState(props.ingredients);
+
+  function deleteItem(index) {
+    setIngredients(ingredients.filter((ingredient, idx) => idx != index));
+  }
 
   return (
     <div className="col-5">
-      <ul class="list-group">
-        {props.ingredients.map((ingredient) => (
-          <li class="list-group-item">
-            {ingredient}
-            <button
-              className={hovered ? "visible": "invisible"}
-              onMouseEnter={toggleHover}
-              onMouseLeave={toggleHover}
-            ><i class="bi bi-trash"></i>
-            </button>
+      <ul className="list-group">
+        {ingredients.map((ingredient, index) => (
+          <li
+            key={index}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+            className="d-flex list-group-item"
+          >
+            <span>{ingredient}</span>
+            <div className="ms-auto">
+              <i
+                onClick={() => deleteItem(index)}
+                className={
+                  hovered ? "visible fas fa-ban ms-auto" : "invisible "
+                }
+              ></i>
+            </div>
           </li>
         ))}
       </ul>
