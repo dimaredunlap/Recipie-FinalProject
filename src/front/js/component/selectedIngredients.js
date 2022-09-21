ddf90cf84850f8b719c9eb5084167d3c311f7d35
idhanani786 <irshandhanani786@gmail.com>
2022-09-21 19:54:06 +0000

    btn

--- src/front/js/component/selectedIngredients.js
+++ src/front/js/component/selectedIngredients.js
@@ -9,23 +9,33 @@ export const SelectedIngredients = (props) => {
     setIngredients(ingredients.filter((ingredient, idx) => idx != index));
   }
 
+  function visibilityChanger(index){
+    let element= document.getElementById("icon"+index)
+      // if(element.classList.("invisible")){
+      //   element.classList= "visible fas fa-ban ms-auto"
+      // }
+      // else{
+      //   element.classList= "invisible"
+      // }
+      element.classList.toggle("visible")
+  }
+
+
   return (
     <div className="col-5">
       <ul className="list-group">
         {ingredients.map((ingredient, index) => (
           <li
             key={index}
-            onMouseEnter={() => setHovered(true)}
-            onMouseLeave={() => setHovered(false)}
+            onMouseEnter={() => visibilityChanger(index)}
+            onMouseLeave={() => visibilityChanger(index)}
             className="d-flex list-group-item"
           >
             <span>{ingredient}</span>
             <div className="ms-auto">
               <i
+              id={"icon"+index}
                 onClick={() => deleteItem(index)}
-                className={
-                  hovered ? "visible fas fa-ban ms-auto" : "invisible "
-                }
               ></i>
             </div>
           </li>
